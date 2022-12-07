0e541450c30e1d031e3b877eabb9f980c5a4b3a5
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-08 00:19:21 +0100

    Adding create category method and test

---
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
@@ -0,0 +1,37 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.Flow;
+
+@RestController
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories/")
+    Flux<Category> getCategoryList(){
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> getCategoryList(@PathVariable String id){
+        return categoryRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories/")
+    Mono<Void> createCategory(@RequestBody Publisher<Category> categoryStream){
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+}
---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,72 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+
+class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @BeforeEach
+    public void setUp(){
+        categoryRepository= Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient=WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void getAll(){
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(
+                        Category.builder().description("Cat1").build(),
+                        Category.builder().description("Cat2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+
+    }
+
+    @Test
+    public void getById(){
+        BDDMockito.given(categoryRepository.findById("someID"))
+                .willReturn(Mono.just(
+                        Category.builder().description("Cat").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/someID")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(1);
+    }
+
+    @Test
+    public void testCreateCategory(){
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().description("Cat").build()));
+
+        Mono<Category> catToSave =Mono.just(Category.builder().description("Some cat").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories/")
+                .body(catToSave, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+}
