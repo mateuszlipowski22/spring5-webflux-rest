5a073201cc4507c5a37fceb4eb8de3dbf4ed4648
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-08 00:52:18 +0100

    Adding create and update vendor (method and test)

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -4,9 +4,9 @@ import guru.springframework.spring5webfluxrest.domain.Category;
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,5 +27,17 @@ public class VendorController {
     @GetMapping("/api/v1/vendors/{id}")
     Mono<Vendor> getCategoryList(@PathVariable String id){
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/vendors/")
+    Mono<Void> createCategory(@RequestBody Publisher<Vendor> vendorStream){
+        return vendorRepository.saveAll(vendorStream).then();
+    }
+
+    @PutMapping("/api/v1/vendors/{id}")
+    Mono<Vendor> createCategory(@RequestBody Vendor vendor, @PathVariable String id){
+        vendor.setId(id);
+        return vendorRepository.save(vendor);
     }
 }
--- src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 class VendorControllerTest {
 
@@ -28,7 +30,7 @@ class VendorControllerTest {
     }
 
     @Test
-    void getCategoryList() {
+    void getVendorList() {
         BDDMockito.given(vendorRepository.findAll())
                 .willReturn(Flux.just(
                         Vendor.builder().firstName("Cat1").build(),
@@ -42,7 +44,7 @@ class VendorControllerTest {
     }
 
     @Test
-    void testGetCategoryList() {
+    void testGetVendorList() {
         BDDMockito.given(vendorRepository.findById("someID"))
                 .willReturn(Mono.just(
                         Vendor.builder().firstName("Cat1").build()));
@@ -52,5 +54,35 @@ class VendorControllerTest {
                 .exchange()
                 .expectBodyList(Vendor.class)
                 .hasSize(1);
+    }
+
+    @Test
+    public void testCreateVendor(){
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().firstName("Cat").build()));
+
+        Mono<Vendor> vendorToSave =Mono.just(Vendor.builder().firstName("Some vend").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors/")
+                .body(vendorToSave, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void testUpdateVendor(){
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().firstName("Update").build()));
+
+        Mono<Vendor> vendorToUpdate =Mono.just(Vendor.builder().firstName("Some cat").build());
+
+        webTestClient.put()
+                .uri("/api/v1/vendors/dsada")
+                .body(vendorToUpdate, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
